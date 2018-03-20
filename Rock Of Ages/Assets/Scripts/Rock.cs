ea289d91d80e4b8445bd25ffa89df6f2dfa34f4c
Kendolf <ikatuev@yandex.ru>
2018-03-20 19:34:52 +0400

    Initial commit

---
+++ Rock Of Ages/Assets/Scripts/Rock.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        // apply impulse force to get game object moving
+        const float MinImpulseForce = 1f;
+        const float MaxImpulseForce = 2f;
+        float angle = Random.Range( 0, 2 * Mathf.PI );
+        Vector2 direction = new Vector2( Mathf.Cos(angle), Mathf.Sin( angle ) );
+        float magnitude = Random.Range(MinImpulseForce, MaxImpulseForce);
+        GetComponent<Rigidbody2D>().AddForce( direction * magnitude, ForceMode2D.Impulse );
+
+        // let's get some rotation going
+        float rotation = Random.Range( -10, 10);
+        GetComponent<Rigidbody2D>().AddTorque( rotation );
+    }
+	
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
